bca895792f4b5db8f267bce8863a02eb0c20c8c4
jamiemalcolm <jmalcolm281@gmail.com>
2021-01-07 17:15:10 +0000

    some sudo code to begin

--- models/traveller.js
+++ models/traveller.js
@@ -3,23 +3,24 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  // get map of journeys and return journeys.startLocation  
+  
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  // get map of journeys and return journeys.endLocation
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  // filter journeys return new array 
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  // filter journeys return new array
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  // reduce on journeys 
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
