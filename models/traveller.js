c625a2a4c749f39c058da7df20379485e133b195
jamiemalcolm <jmalcolm281@gmail.com>
2021-01-07 17:34:20 +0000

    function for mover a certain distance written

--- models/traveller.js
+++ models/traveller.js
@@ -28,6 +28,10 @@ Traveller.prototype.getJourneysByTransport = function (transport) {
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
   // filter journeys return new array
+  const bigJourney = this.journeys.filter(journey => {
+    return (journey.distance >= minDistance);
+  });
+  return bigJourney;
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
